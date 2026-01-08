f7145c1345a5452eb1456825a3a00b327792bb19
Anton Ekblad <anton@ekblad.cc>
2026-01-09 00:08:54 +0100

    Set up APK signing.

---
+++ sign.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+set -eo pipefail
+read -p "Enter Yubikey PIN: " -s PIN
+apksigner \
+    "-J-add-exports jdk.crypto.cryptoki/sun.security.pkcs11=ALL-UNNAMED" \
+    sign --ks NONE \
+    --ks-pass "pass:$PIN" \
+    --provider-class sun.security.pkcs11.SunPKCS11 \
+    --provider-arg ./signing-pkcs11-provider.cfg \
+    --ks-type PKCS11 \
+    --min-sdk-version 31 \
+    --max-sdk-version 36 \
+    --in app/build/outputs/apk/release/app-release-unsigned.apk \
+    --out snout-authenticator.apk
