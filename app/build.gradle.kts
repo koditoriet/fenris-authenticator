427a8731ebad194c8902cff38f89e04d7c82ad81
Anton Ekblad <anton@ekblad.cc>
2026-03-18 01:07:51 +0100

    Bump version.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,7 +27,7 @@ android {
         minSdk = 31
         targetSdk = 36
         versionCode = 1
-        versionName = "0.1-pre9"
+        versionName = "0.1-pre10"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
