9be76e9fb20301ecba8e290eb9070c6a220fb89b
Anton Ekblad <anton@ekblad.cc>
2026-04-07 20:33:00 +0200

    Set up signing for both APK and AAB. (#89)

---
+++ build-release.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+set -eo pipefail
+APP_NAME="fenris-authenticator"
+VERSION="$(./gradlew -q printVersionName)"
+APK_NAME="${APP_NAME}_${VERSION}.apk"
+AAB_NAME="${APP_NAME}_${VERSION}.aab"
+
+./gradlew clean assembleRelease bundleRelease
+
+# apksigner breaks if there's more than one key per PKCS11 slot,
+# so we make sure to only use one "standard" yubikey slot and the opensc pkcs11
+# provider, which can only see the standard slots, to hide the other
+# signing key from apksigner.
+./sign-apk.sh ./sign-apk.cfg "$APK_NAME"
+
+# jarsigner, meanwhile, is not broken like apksigner, so we use the ykcs11
+# provider and stick the aab upload signing key in a non-standard (i.e. 0x83)
+# yubikey slot.
+./sign-aab.sh ./sign-aab.cfg "$AAB_NAME"
