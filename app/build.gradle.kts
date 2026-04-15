07d6954246c954bef8c3d8102a30e2a2a8335801
Anton Ekblad <anton@ekblad.cc>
2026-04-16 01:14:39 +0200

    Remove duplicate material3 dependency.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -87,7 +87,6 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
-    implementation(libs.androidx.material3)
     implementation(libs.androidx.biometric.ktx)
     implementation(libs.androidx.compose.material.icons.extended)
     implementation(libs.androidx.datastore)
